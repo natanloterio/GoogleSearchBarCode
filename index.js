f5040079e9b4621d9a909434d88b0350f93d536e
Natan Loterio <natan@MacBook-Pro-Natan.local>
2020-01-24 18:05:46 -0300

    adding express support

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ const robot = {
     translateBarcodeToName: require('./translator'),
 }
 
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/:b', (req, res) => barcodeToName(req,res) )
+
 async function start() {
     const result = await robot.translateBarcodeToName('7804630540041')
     console.dir(result, {depth: null})
 }
 
-start()
+async function barcodeToName(req, res) {
+    const barCode = req.params.b
+    console.log(`Fetching up barcode: ${barCode}`)
+    const result = await robot.translateBarcodeToName(barCode)
+    res.send(result)
+}
+
+
+app.listen(port, () =>console.log(`Running on ${port}`))
